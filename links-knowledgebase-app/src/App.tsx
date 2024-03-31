20612d181716fb34528b07cdaa69624a4e6a50f6
Georgii Skoblev <gskobelev@pm.me>
2024-03-31 14:44:54 +0400

    last update of frontend mvp

--- links-knowledgebase-app/src/App.tsx
+++ links-knowledgebase-app/src/App.tsx
@@ -1,16 +1,16 @@
 // App.js
-import 'normalize.css'
-import '@mantine/core/styles.css';
-import { createBrowserRouter, RouteObject, RouterProvider } from "react-router-dom";
+import "normalize.css";
+import "@mantine/core/styles.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { FolderComponent } from "./components/FolderComponent";
-import { FileComponent } from "./components/FileComponent";
 import { AppShell } from "@mantine/core";
 import { MainLayout } from "./layouts/MainLayout";
-import { Folder } from './types/folder';
+import { Folder } from "./types/folder";
+import { getAllFilesAndFolders } from "./utils/getAllFilesAndFolders";
 
 const folderStructure: Folder = {
   name: "Root",
-  type: 'folder',
+  type: "folder",
   children: [
     {
       type: "folder",
@@ -36,49 +36,28 @@ const folderStructure: Folder = {
   ],
 };
 
-// Helper function to flatten the folder structure and generate routes
-function getAllFilesAndFolders(folder: Folder, path = "") {
-  let routes: RouteObject[] = [];
-  folder.children.forEach((child) => {
-    // Ensure the path is absolute
-    const childPath = `/${path}/${child.name}`.replace(/\/+/g, "/");
-    if (child.type === "file") {
-      routes.push({ path: childPath, element: <FileComponent {...child} /> });
-    } else {
-      routes.push({
-        path: childPath,
-        element: <FolderComponent {...child} path={childPath} />,
-      });
-      // Recursively generate routes, passing down the absolute path
-      routes = routes.concat(getAllFilesAndFolders(child, childPath));
-    }
-  });
-  return routes;
-}
-
-// Define the root routes and dynamically generated routes from the folder structure
-const routes = [
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        path: "",
-        element: <FolderComponent {...folderStructure} path="" />, // Root path to render the base folder component
-      },
-      ...getAllFilesAndFolders(folderStructure), // Spread the dynamically generated routes for files and folders
-    
-    ]
-  },
-  {
-    path: "*",
-    element: <div>404 Not Found</div>, // Fallback for unmatched routes
-  },
-];
-
-const router = createBrowserRouter(routes);
-
 const App = () => {
+  // Define the root routes and dynamically generated routes from the folder structure
+  const routes = [
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "",
+          element: <FolderComponent {...folderStructure} path="" />, // Root path to render the base folder component
+        },
+        ...getAllFilesAndFolders(folderStructure), // Spread the dynamically generated routes for files and folders
+      ],
+    },
+    {
+      path: "*",
+      element: <div>404 Not Found</div>, // Fallback for unmatched routes
+    },
+  ];
+
+  const router = createBrowserRouter(routes);
+
   return (
     <AppShell>
       <RouterProvider router={router} />
